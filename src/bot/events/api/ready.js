24eccab57836cb33559d0c977a8f05e91f43e1b0
Glitch (shadowed-diagnostic-sweatpants) <none>
2022-04-26 18:53:02 +0000

    📍🏯 Checkpoint ./package.json:44178510/19574 ./src/bot/events/api/ready.js:44178510/946 ./src/index.js:44178510/4027

--- src/bot/events/api/ready.js
+++ src/bot/events/api/ready.js
@@ -2,8 +2,8 @@ const chalk = require('chalk');
 const figlet = require("figlet");
 
 module.exports = async function(client) {
-    console.log(chalk.yellow.bold(figlet.textSync("Rexom")));
-    await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold(" Is Ready"));
+    console.log(chalk.yellow.bold(figlet.textSync("Rechard")));
+    await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold("Bot Aktif"));
 
     await client.user.setActivity(require('../../../config/bot').prefix + "help");
     await client.user.setStatus("idle");
